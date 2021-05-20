d7f98f80b4f9096858b6dbf20d17f51905788852
Tamara <tabualhsan@gmail.com>
2021-05-19 21:52:37 -0700

    adding some styling, need to center recipes in components

--- src/App.js
+++ src/App.js
@@ -5,33 +5,61 @@ const App = () => {
 
   const APP_ID = '6ca2a4cb';
   const APP_KEY ='b3937da8322b43a99883284299e4738e';
+  const [search, setSearch] = useState('');
+  //state componet that gets passed to the prop compnent below
   const [recipes, setRecipes] = useState([]);
+  // to fetch data in search bar
+  const [query, setQuery] = useState('chicken');
+
   
 
   useEffect( () => {
-
     getRecipes();
+  }, [query]);
 
-   
-  }, []);
+  // only work when you submit^
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
     const data = await response.json();
     setRecipes(data.hits);
    
 
+  };
+
+  const updateSearch = e =>{
+    setSearch(e.target.value);
+
+
+  }
+
+  const getSearch = e =>{
+    // stops page refresh 
+    e.preventDefault();
+    setQuery(search);
+    getSearch('');
+    // the search bar will go back to empty ^
   }
 
   return(
   <div className="app">
-    <form className="search-form">
-      <input className="search-bar" type="text"/>
+    <form onSubmit={getSearch} className="search-form">
+      <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
       <button className="search-button" type="submit"> search </button>
     </form>  
+
+    {/* props */}
+    <div className="recipes">
     {recipes.map(recipe =>(
-      <Recipe />
+      <Recipe 
+      key={recipe.recipe.label}
+      title={recipe.recipe.label} 
+      calories={recipe.recipe.calories}
+      image={recipe.recipe.image} 
+      ingredients={recipe.recipe.ingredients}
+      />
     ))}
+    </div>
   </div>
   );
 };
